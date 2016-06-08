c068a2f0325b61c19bc5472fa5a32cdd8ee942c9
mtj1041 <mtjoseph@berkeley.edu>
2016-06-08 09:35:32 +0000

    Changed root directory

--- gitlet_hub/urls.py
+++ gitlet_hub/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^hub/', include('hub.urls')),
+    url(r'^/', include('hub.urls')),
     url(r'^admin/', admin.site.urls),
 ]
