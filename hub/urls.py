64f28ff01d1cbd976e020c5593392cbb20e0083e
mtj1041 <mtjoseph@berkeley.edu>
2016-06-09 06:28:14 +0000

    Made login CSRF exempt to allow 3rd party interaction

--- hub/urls.py
+++ hub/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
@@ -6,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^panel/([0-9]+)/$', views.panel, name='panel'),
     url(r'^register/$', views.register, name='register'),
-    url(r'login/', views.login, name='login'),
+    url(r'login/', csrf_exempt(views.login), name='login'),
     url(r'logout/', views.logout, name='logout'),
     url(r'success/', views.success, name='success'),
     url(r'failure/', views.failure, name='failure'),
